ab902da7d4a5e187cf8271cf971ee3f2f0499e28
Jacques <jacques.laffy@gmail.com>
2019-02-11 15:01:35 +0000

    First Feature written - User can Deposit

---
+++ spec/features/UserCanDepositSpec.js
@@ -0,0 +1,12 @@
+describe ('User Can Deposit Money', function () {
+  var account;
+
+  beforeEach (function () {
+    account = new Account;
+  });
+
+  it('A user should be able to deposit money in his account', function () {
+    account.deposit(100);
+    expect(account.balance).toEqual(100)
+  });
+});
