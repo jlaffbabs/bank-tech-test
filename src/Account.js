e223135988de62f7ffc8fd1e4b30ff253a9e69c4
Jacques <jacques.laffy@gmail.com>
2019-02-11 15:56:19 +0000

    Passing features - haven't linked transactions (or stored) yet

--- src/Account.js
+++ src/Account.js
@@ -2,4 +2,12 @@ class Account {
   constructor () {
     this.balance = 0.00;
   };
+
+  deposit (amount) {
+    this.balance += amount;
+  };
+
+  withdraw (amount) {
+    this.balance -= amount;
+  };
 };
