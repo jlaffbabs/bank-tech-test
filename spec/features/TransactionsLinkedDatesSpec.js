88a126fc22ebda4115b1787b528410e8615ba2c0
Jacques <jacques.laffy@gmail.com>
2019-02-14 15:34:57 +0000

    Mocked date in test

--- spec/features/TransactionsLinkedDatesSpec.js
+++ spec/features/TransactionsLinkedDatesSpec.js
@@ -1,13 +1,13 @@
 describe('Transactions are attached to a date', function () {
   var account
+  var txn1 = {amount: 320.00, date: new Date("2010-08-19"), type: 'credit', balance: 5000.00}
 
   beforeEach(function () {
     account = new Account()
   })
 
   it('A transaction should have a date attached for statement', function () {
-    account.deposit(320.00)
-    var today = new Date()
-    expect(account.statementCreate()).toContain(today.toLocaleDateString('en-GB'))
+    account.transactions.push(txn1)
+    expect(account.statementCreate()).toContain("19/08/2010")
   })
 })
